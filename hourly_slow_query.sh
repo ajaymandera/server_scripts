a80dae14d4ad6e487b61119bb187e97146cdfea1
ajaymandera <ajay.mandera@plus91.in>
2016-01-06 13:05:37 +0530

    TO: email id updated

--- hourly_slow_query.sh
+++ hourly_slow_query.sh
@@ -7,7 +7,7 @@ do
 	if [ $(wc -l </var/log/mysql/mysql-slow.log) -gt $count ]
 	then
 	#mail -s "$server_name Server - Slow Query Log " $slow_query_email_ids < /var/log/mysql/mysql-slow.log
-	echo "Slow Query" | mailx -r "ajay.mandera@plus91.in" -s "$server_name Server - Slow Query Log" -S smtp="$smtp_server" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="$smtp_user" -S smtp-auth-password="$smtp_password" -S ssl-verify=ignore $email_ids < /var/log/mysql/mysql-slow.log
+	echo "Slow Query" | mailx -r "ajay.mandera@plus91.in" -s "$server_name Server - Slow Query Log" -S smtp="$smtp_server" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="$smtp_user" -S smtp-auth-password="$smtp_password" -S ssl-verify=ignore $slow_query_email_ids < /var/log/mysql/mysql-slow.log
 	count=$(wc -l < /var/log/mysql/mysql-slow.log)
 	fi
 	sleep 1h
