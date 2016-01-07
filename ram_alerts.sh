3b91005e007f56bddfd82d4d345583a29071f51a
ajaymandera <ajay.mandera@plus91.in>
2016-01-07 11:00:10 +0530

    Ram Alerts condition changed

--- ram_alerts.sh
+++ ram_alerts.sh
@@ -12,7 +12,7 @@ then
 	ram2=0
 fi
 
-if [[ $ram1 > "75"  &&  $ram2 < "75" ]]
+if [[ $ram1 > "75"  &&  $ram2 > "75" ]]
 then
 ram2=$(awk 'BEGIN{print '$total_ram'*'$ram2/100'}')
 mysql --defaults-file="/plus91/mysql.txt" $database << EOF
@@ -21,4 +21,3 @@ EOF
 #echo "RAM Usage high on $server_name Server, Current RAM usage $ram2" | mail -s "$server_name Server - RAM Usage High" $email_ids
 echo "RAM Usage high on $server_name Server, Current RAM usage $ram2" | mailx -r "ajay.mandera@plus91.in" -s "$server_name Server - RAM Usage High" -S smtp="$smtp_server" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="$smtp_user" -S smtp-auth-password="$smtp_password" -S ssl-verify=ignore $email_ids
 fi
-
